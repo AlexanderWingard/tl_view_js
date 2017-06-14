cc47c2029b1e1a11ad2b5e3926ba99a4d942857a
Alexander Wingård <alexander.wingard@gmail.com>
2017-06-15 01:28:45 +0200

    Compute timediff

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+var focus;
+function recalculate(data) {
+    for(var i = 0; i < data.length; i++) {
+        var prev = get_previous_ts(data, i);
+        if(prev != undefined) {
+            data[i]["cols"][0] = format_timediff(data[i]["ts"], prev);
+        }
+    }
+}
+function format_timediff(now, then) {
+    return timeConversion(now.diff(then));
+}
+
+function get_previous_ts(data, index) {
+    for(var i = index-1; i >= 0; i--) {
+        if(data[i]["ts"].isValid()) {
+            return data[i]["ts"];
+        }
+    }
+}
+
+function timeConversion(millisec) {
+    var seconds = (millisec / 1000).toFixed(0);
+    var minutes = (millisec / (1000 * 60)).toFixed(1);
+    var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+    if (seconds < 60) {
+        return seconds + " s";
+    } else if (minutes < 60) {
+        return minutes + " m";
+    } else if (hours < 24) {
+        return hours + " h";
+    } else {
+        return days + " d";
+    }
+}
+
 function render(data) {
+    recalculate(data);
     var result = d3.select("#result");
 
     var select = result
